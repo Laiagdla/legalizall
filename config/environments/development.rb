cff0f041238dce79ba215c344a45853ec49d9902
Laiagdla <25137442+Laiagdla@users.noreply.github.com>
2024-10-29 14:04:17 +0100

    basic structure

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
+  # for avoiding precompiling
+  # config.action_controller.perform_caching = false
+
   # Do not eager load code on boot.
   config.eager_load = false
 
